51da5de99a8953d7e1f52d82c7c4cb60879b7274
Vani Sidagam <vanisdgm@gmail.com>
2021-12-17 16:00:26 +0530

    Create MainClass.java

---
+++ devops-github-packages-main/src/main/java/io/flowsquad/blog/devops/github/packages/main/MainClass.java
@@ -0,0 +1,9 @@
+package io.vanisidagam.blog.devops.github.packages.main;
+
+import io.vanisidagam.blog.devops.github.packages.library.LibraryClass;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println(new LibraryClass().utilityMethod());
+    }
+}
